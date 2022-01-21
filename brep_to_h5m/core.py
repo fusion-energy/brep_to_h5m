4ebab41cada07d2e9992808c43fb3deab5889d31
Jonathan <mail@jshimwell.com>
2022-01-21 20:10:26 +0000

    added doc strings

--- brep_to_h5m/core.py
+++ brep_to_h5m/core.py
@@ -1,7 +1,9 @@
+import tempfile
+
+import brep_part_finder as bpf
 import gmsh
 import trimesh
 from stl_to_h5m import stl_to_h5m
-import brep_part_finder as bpf
 
 
 def brep_to_h5m(
@@ -11,11 +13,36 @@ def brep_to_h5m(
     min_mesh_size: int = 30,
     max_mesh_size: int = 10,
     mesh_algorithm: int = 1,
+    write_stl_files_to_temp: bool = True,
+    delete_intermediate_stl_files: bool = True,
 ):
+    """Converts a Brep file into a DAGMC h5m file. This makes use of Gmsh and
+    will therefore need to have Gmsh installed to work.
+
+    Args:
+        brep_filename: the filename of the Brep file to convert
+        volumes_with_tags: a dictionary with volume numbers as the keys and
+            the tag names to use in DAGMC as te values.
+        h5m_filename: the filename of the DAGMC h5m file to write
+        min_mesh_size: the minimum mesh element size to use in Gmsh. Passed
+            into gmsh.option.setNumber("Mesh.MeshSizeMin", min_mesh_size)
+        max_mesh_size: the maximum mesh element size to use in Gmsh. Passed
+            into gmsh.option.setNumber("Mesh.MeshSizeMax", max_mesh_size)
+        mesh_algorithm: The Gmsh mesh algorithm number to use. Passed into
+            gmsh.option.setNumber("Mesh.Algorithm", mesh_algorithm)
+        write_stl_files_to_temp: If set to True the intermediate STL files
+            required will be written to the opperating systems temporary file
+            folder. If set to False the STL files will be written to the
+            current working directory.
+        delete_intermediate_stl_files: If set to True the intermediate STL
+            files produced will be deleted. If set the False the intermediate
+            STL files will be left intact.
+
+    """
 
     gmsh.initialize()
     gmsh.option.setNumber("General.Terminal", 1)
-    gmsh.model.add("made_with_brep_to_h5m")
+    gmsh.model.add("made_with_brep_to_h5m_package")
     volumes = gmsh.model.occ.importShapes(brep_filename)
     gmsh.model.occ.synchronize()
     gmsh.option.setNumber("Mesh.Algorithm", mesh_algorithm)
@@ -29,8 +56,12 @@ def brep_to_h5m(
         surfaces_in_volume = entities_in_volume[1]
         ps = gmsh.model.addPhysicalGroup(2, surfaces_in_volume)
         gmsh.model.setPhysicalName(2, ps, f"surfaces_on_volume_{vol_id}")
-        gmsh.write(f"volume_{vol_id}.stl")
-        stl_filenames.append((vol_id, f"volume_{vol_id}.stl"))
+        if write_stl_files_to_temp:
+            tmp_filename = tempfile.mkstemp(suffix=".stl", prefix=f"volume_{vol_id}")[1]
+        else:
+            tmp_filename = f"volume_{vol_id}.stl"
+        gmsh.write(tmp_filename)
+        stl_filenames.append((vol_id, tmp_filename))
         gmsh.model.removePhysicalGroups([])  # removes all groups
     gmsh.finalize()
 
@@ -46,6 +77,10 @@ def brep_to_h5m(
             )  # reqired as gmsh stl export from brep can get the inside outside mixed up
             new_filename = filename[:-4] + "_with_corrected_face_normals.stl"
             mesh.export(new_filename)
+            import os
+
+            if delete_intermediate_stl_files:
+                os.remove(filename)  # deletes tmp stl file
             tag_name = volumes_with_tags[vol_id]
             if not tag_name.startswith("mat_"):
                 # TODO check if graveyard or mat_graveyard should be excluded
@@ -54,6 +89,11 @@ def brep_to_h5m(
             files_with_tags.append((new_filename, tag_name))
 
     stl_to_h5m(files_with_tags=files_with_tags, h5m_filename=h5m_filename)
+
+    # deletes the stl with corrected faces file from the tmp dir
+    if delete_intermediate_stl_files:
+        for file_to_del in files_with_tags:
+            os.remove(file_to_del[0])
 
     return h5m_filename
 
