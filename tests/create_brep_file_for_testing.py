4a4dde4d64eb580892f1890ec6a75af0aa39feca
Jonathan Shimwell <mail@jshimwell.com>
2022-06-17 14:25:45 +0100

    added two sep cubes

--- tests/create_brep_file_for_testing.py
+++ tests/create_brep_file_for_testing.py
@@ -25,3 +25,27 @@ my_cube_2 = paramak.ExtrudeStraightShape(
 )
 my_cubes = paramak.Reactor([my_cube_1, my_cube_2])
 my_cubes.export_brep("test_two_cubes.brep")
+
+
+my_cube_3 = paramak.ExtrudeStraightShape(
+    name="my_cube_3",
+    points=[
+        [0, 0],
+        [0, 10],
+        [10, 10],
+        [10, 0],
+    ],
+    distance=5,
+)
+my_cube_4 = paramak.ExtrudeStraightShape(
+    name="my_cube_4",
+    points=[
+        [-1, 0],
+        [-1, 10],
+        [-10, 10],
+        [-10, 0],
+    ],
+    distance=5,
+)
+my_cubes = paramak.Reactor([my_cube_3, my_cube_4])
+my_cubes.export_brep("test_two_sep_cubes.brep")
