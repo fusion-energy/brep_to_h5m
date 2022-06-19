2fd2ded5aabe86386a943fc9903546fab561fa23
Jonathan Shimwell <mail@jshimwell.com>
2022-06-19 16:19:02 +0100

    crashing with obb setup error

---
+++ examples/h5m_from_coords/make_h5m_from_coords.py
@@ -0,0 +1,141 @@
+
+import numpy as np
+import gmsh
+import trimesh
+from pymoab import core, types
+from typing import Tuple
+
+
+def _define_moab_core_and_tags() -> Tuple[core.Core, dict]:
+    """Creates a MOAB Core instance which can be built up by adding sets of
+    triangles to the instance
+
+    Returns:
+        (pymoab Core): A pymoab.core.Core() instance
+        (pymoab tag_handle): A pymoab.core.tag_get_handle() instance
+    """
+
+    # create pymoab instance
+    moab_core = core.Core()
+
+    tags = dict()
+
+    sense_tag_name = "GEOM_SENSE_2"
+    sense_tag_size = 2
+    tags["surf_sense"] = moab_core.tag_get_handle(
+        sense_tag_name,
+        sense_tag_size,
+        types.MB_TYPE_HANDLE,
+        types.MB_TAG_SPARSE,
+        create_if_missing=True,
+    )
+
+    tags["category"] = moab_core.tag_get_handle(
+        types.CATEGORY_TAG_NAME,
+        types.CATEGORY_TAG_SIZE,
+        types.MB_TYPE_OPAQUE,
+        types.MB_TAG_SPARSE,
+        create_if_missing=True,
+    )
+
+    tags["name"] = moab_core.tag_get_handle(
+        types.NAME_TAG_NAME,
+        types.NAME_TAG_SIZE,
+        types.MB_TYPE_OPAQUE,
+        types.MB_TAG_SPARSE,
+        create_if_missing=True,
+    )
+
+    tags["geom_dimension"] = moab_core.tag_get_handle(
+        types.GEOM_DIMENSION_TAG_NAME,
+        1,
+        types.MB_TYPE_INTEGER,
+        types.MB_TAG_DENSE,
+        create_if_missing=True,
+    )
+
+    # Global ID is a default tag, just need the name to retrieve
+    tags["global_id"] = moab_core.tag_get_handle(types.GLOBAL_ID_TAG_NAME)
+
+    return moab_core, tags
+
+
+
+
+moab_core, tags = _define_moab_core_and_tags()
+
+
+surface_id = 1
+volume_id = 1
+
+# this will need repeating for every volume in the dagmc model
+# with different volume surface ids
+######### repeat this section
+surface_set = moab_core.create_meshset()
+volume_set = moab_core.create_meshset()
+
+# recent versions of MOAB handle this automatically
+# but best to go ahead and do it manually
+moab_core.tag_set_data(tags["global_id"], volume_set, volume_id)
+
+moab_core.tag_set_data(tags["global_id"], surface_set, surface_id)
+
+# set geom IDs
+moab_core.tag_set_data(tags["geom_dimension"], volume_set, 3)
+moab_core.tag_set_data(tags["geom_dimension"], surface_set, 2)
+
+# set category tag values
+moab_core.tag_set_data(tags["category"], volume_set, "Volume")
+moab_core.tag_set_data(tags["category"], surface_set, "Surface")
+
+# establish parent-child relationship
+moab_core.add_parent_child(volume_set, surface_set)
+
+# set surface sense
+sense_data = [volume_set, np.uint64(0)]
+moab_core.tag_set_data(tags["surf_sense"], surface_set, sense_data)
+
+#######
+
+# coordinates of the first volume
+coords = np.array([
+    [
+            [0,0,0], [1,0,0], [0,1,0],  # bottom face 
+    ],
+    [
+            [0,0,1], [1,0,0], [0,1,0],  # front face
+    ],
+    [
+            [0,0,0], [0,1,0], [0,0,1],  # back face
+    ],
+    [
+            [0,0,0], [1,0,0], [0,0,1],  # left face
+    ],
+    ], dtype='float64')
+
+
+for three_coord_face in coords:
+    print('adding ', three_coord_face, 'to moab')
+    verts = moab_core.create_vertices(three_coord_face)
+
+    triangle = moab_core.create_element(types.MBTRI,verts)
+
+    group_set = moab_core.create_meshset()
+
+    moab_core.tag_set_data(tags["category"], group_set, "Group")
+
+    moab_core.tag_set_data(tags["name"], group_set, 'mat:dag_material_tag')
+
+    moab_core.tag_set_data(tags["geom_dimension"], group_set, 4)
+
+    moab_core.add_entity(group_set, volume_set)
+
+
+
+all_sets = moab_core.get_entities_by_handle(0)
+
+file_set = moab_core.create_meshset()
+
+moab_core.add_entities(file_set, all_sets)
+
+moab_core.write_file('dagmc_from_coords.h5m')
---
+++ examples/h5m_from_coords/test_openmc_simulation.py
@@ -0,0 +1,75 @@
+
+import openmc
+import openmc_data_downloader as odd
+import math
+# simplified material definitions have been used to keen this example minimal
+mat_dag_material_tag = openmc.Material(name="dag_material_tag")
+mat_dag_material_tag.add_element("H", 1, "ao")
+mat_dag_material_tag.set_density("g/cm3", 0.00001)
+
+materials = openmc.Materials(
+    [
+        mat_dag_material_tag,
+    ]
+)
+
+# downloads the nuclear data and sets the openmc_cross_sections environmental variable
+odd.just_in_time_library_generator(libraries="ENDFB-7.1-NNDC", materials=materials)
+
+# makes use of the dagmc geometry
+dag_univ = openmc.DAGMCUniverse("dagmc_from_coords.h5m")
+
+# creates an edge of universe boundary surface
+vac_surf = openmc.Sphere(r=10000, surface_id=9999, boundary_type="vacuum")
+
+# specifies the region as below the universe boundary
+region = -vac_surf
+
+# creates a cell from the region and fills the cell with the dagmc geometry
+containing_cell = openmc.Cell(cell_id=9999, region=region, fill=dag_univ)
+
+geometry = openmc.Geometry(root=[containing_cell])
+
+# initialises a new source object
+my_source = openmc.Source()
+# sets the location of the source to x=0 y=0 z=0
+my_source.space = openmc.stats.Point((0, 0, 0))
+# sets the direction to isotropic
+my_source.angle = openmc.stats.Isotropic()
+# sets the energy distribution to 100% 14MeV neutrons
+my_source.energy = openmc.stats.Discrete([14e6], [1])
+
+
+# specifies the simulation computational intensity
+settings = openmc.Settings()
+settings.batches = 10
+settings.particles = 10000
+settings.inactive = 0
+settings.run_mode = "fixed source"
+settings.source = my_source
+
+# adds a tally to record the heat deposited in entire geometry
+cell_tally = openmc.Tally(name="heating")
+cell_tally.scores = ["heating"]
+
+# creates a mesh that covers the geometry
+mesh = openmc.RegularMesh()
+mesh.dimension = [100, 100, 100]
+mesh.lower_left = [-10, -10, -10]  # x,y,z coordinates start at 0 as this is a sector model
+mesh.upper_right = [10, 10, 10]
+
+# makes a mesh tally using the previously created mesh and records heating on the mesh
+mesh_tally = openmc.Tally(name="heating_on_mesh")
+mesh_filter = openmc.MeshFilter(mesh)
+mesh_tally.filters = [mesh_filter]
+mesh_tally.scores = ["heating"]
+
+# groups the two tallies
+tallies = openmc.Tallies([cell_tally, mesh_tally])
+
+# builds the openmc model
+my_model = openmc.Model(materials=materials, geometry=geometry, settings=settings, tallies=tallies)
+
+# starts the simulation
+my_model.run()
+
