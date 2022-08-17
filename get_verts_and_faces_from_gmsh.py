e9a7c6a8e86a52c0b8512e46621bcd6bee33dd8e
shimwell <mail@jshimwell.com>
2022-08-17 22:48:21 +0100

    added verts to h5m part

--- get_verts_and_faces_from_gmsh.py
+++ get_verts_and_faces_from_gmsh.py
@@ -1,10 +1,12 @@
-import os
-import tempfile
+# import os
+# import tempfile
+import imp
 import warnings
 
 import gmsh
 import trimesh
-from brep_to_h5m import mesh_brep
+# from vertices_to_h5m import vertices_to_h5m
+# from brep_to_h5m import messh_brep
 
 
 # gmsh, volumes = mesh_brep(
@@ -48,9 +50,10 @@ gmsh.option.setNumber("Mesh.Algorithm", mesh_algorithm)
 gmsh.option.setNumber("Mesh.MeshSizeMin", min_mesh_size)
 gmsh.option.setNumber("Mesh.MeshSizeMax", max_mesh_size)
 gmsh.model.mesh.generate(2)
-    
 
+all_coords = []
 stl_filenames = []
+all_tris=[]
 for dim_and_vol in volumes:
     vol_id = dim_and_vol[1]
     print('vol_id',vol_id)
@@ -58,12 +61,22 @@ for dim_and_vol in volumes:
     surfaces_in_volume = entities_in_volume[1]
     ps = gmsh.model.addPhysicalGroup(2, surfaces_in_volume)
     gmsh.model.setPhysicalName(2, ps, f"surfaces_on_volume_{vol_id}")
-    nodeTags, coords=gmsh.model.mesh.getNodesForPhysicalGroup(dim=2,tag=vol_id)
+    nodeTagsOrg, coords=gmsh.model.mesh.getNodesForPhysicalGroup(dim=2,tag=vol_id)
     n=3
+    coords= coords.tolist()
+    nodeTags = []
+    for tag in nodeTagsOrg:
+        tag=tag-1
+        nodeTags.append(int(tag))
     GroupednodeTags=[nodeTags[i:i+n] for i in range(0, len(nodeTags), n)]
-    print('  nodeTags', nodeTags)
-    print('  coords', coords)
+    
+    GroupednCoords=[coords[i:i+n] for i in range(0, len(coords), n)]
+    # print('  nodeTags', nodeTags)
+    # print('  GroupednodeTags', GroupednodeTags)
+    # print('  coords', coords)
     new_trimesh_object=trimesh.Trimesh(vertices=coords,faces=GroupednodeTags)
+    # new_trimesh_object.show()
+    # trimesh.repair.fix_normals(new_trimesh_object) 
     # e=gmsh.model.getEntitiesForPhysicalGroup(dim=2, tag=1)
     # print('e',e)
     # elementTags, elementNodeTags = gmsh.model.mesh.getElementsByType(3)
@@ -74,6 +87,34 @@ for dim_and_vol in volumes:
     # gmsh.model.mesh.getElementsByCoordinates(x=coords[0], y=coords[1],z=coords[2])?
     # gmsh.write(tmp_filename)
     # stl_filenames.append((vol_id, verts, faces))
-    gmsh.model.removePhysicalGroups([])  # removes all groups
+    # gmsh.model.removePhysicalGroups([])  # removes all groups
+    all_coords = all_coords + GroupednCoords
+    all_tris.append(GroupednodeTags)
+    
 gmsh.finalize()
+
+
+print('all_tris',all_tris)
+print('all_coords',all_coords)
+
+# from vertices_to_h5m import vertices_to_h5m
+# import numpy as np
+# import os
+
+# # These are the x,y,z coordinates of each vertex.
+# # The first 4 are used in the first volume
+# vertices = all_coords
+# # These are the two sets triangle that connect individual vertices together to form a continious surfaces and also two closed volume.
+# triangles = all_tris
+
+# # This will produce a h5m file called two_volume_touching_edge.h5m ready for use with DAGMC enabled codes
+# vertices_to_h5m(
+#     vertices=vertices,
+#     triangles=triangles,
+#     material_tags=["mat1", "mat2"],
+#     h5m_filename="two_volume_touching_edge.h5m",
+# )
+
+# os.system("mbconvert two_volume_touching_edge.h5m two_volume_touching_edge.vtk")
+
 
